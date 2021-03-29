d54c27ed633b40d6cdf12145c0cb487352c57f60
Akira Hayakawa <ruby.wktk@gmail.com>
2021-03-29 20:43:52 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -7,12 +7,35 @@ use uefi::prelude::*;
 extern crate rlibc;
 use core::fmt::Write;
 
+use uefi::{
+    prelude::*,
+    proto::media::fs::SimpleFileSystem,
+    proto::media::file::{File, FileAttribute, FileInfo, FileMode, FileType},
+    table::boot::{AllocateType, MemoryType},
+};
+
+const KERNEL_BASE_ADDR: usize = 0x100000;
+
 #[entry]
 fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
-    let bs = uefi_services::init(&st).expect("Failed to initialize UEFI services");
-    st.stdout()
-        .reset(false)
-        .expect_success("Failed to reset UEFI stdout");
+    uefi_services::init(&st).unwrap_success();
+    st.stdout().reset(false).unwrap_success();
     writeln!(st.stdout(), "Hello, world!").unwrap();
+
+    let boot_services = st.boot_services();
+
+    let fs = boot_services.locate_protocol::<SimpleFileSystem>().unwrap_success();
+    let fs = unsafe { &mut *fs.get() };
+    let mut root_dir = fs.open_volume().unwrap_success();
+ 
+    let fh = root_dir.open("kernel.efi", FileMode::Read, FileAttribute::READ_ONLY).unwrap_success();
+    let file_type = fh.into_type().unwrap_success();
+    if let FileType::Regular(mut f) = file_type {
+        const BUF_SIZE: usize = 4000;
+        let mut buf = [0u8; BUF_SIZE];
+        let info: &mut FileInfo = f.get_info(&mut buf).unwrap_success();
+        let kernel_file_size: u64 = info.file_size();
+        panic!("OK");
+    }
     loop {}
 }
