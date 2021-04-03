42c55ed69fa0617852f2ed53926817b1cdcba92c
Akira Hayakawa <ruby.wktk@gmail.com>
2021-04-03 21:46:23 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -60,18 +60,18 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
     let file_type = fh.into_type().unwrap_success();
     if let FileType::Regular(mut f) = file_type {
         const TMP_BUF_SIZE: usize = 4000;
-        let mut tmp_buf = [0u8; TMP_BUF_SIZE];
-        let info: &mut FileInfo = f.get_info(&mut tmp_buf).unwrap_success();
+        let mut misc_buf = [0u8; TMP_BUF_SIZE];
+        let info: &mut FileInfo = f.get_info(&mut misc_buf).unwrap_success();
         let kernel_file_size: u64 = info.file_size();
 
         // Read kernel file into temporary space
-        let tmp_p = boot_services.allocate_pool(MemoryType::LOADER_DATA, kernel_file_size as usize).unwrap_success();
-        let mut tmp_buf = unsafe { core::slice::from_raw_parts_mut(tmp_p as *mut u8, kernel_file_size as usize) };
-        f.read(&mut tmp_buf).unwrap_success();
+        let kernel_tmp_p = boot_services.allocate_pool(MemoryType::LOADER_DATA, kernel_file_size as usize).unwrap_success();
+        let mut kernel_tmp_buf = unsafe { core::slice::from_raw_parts_mut(kernel_tmp_p as *mut u8, kernel_file_size as usize) };
+        f.read(&mut kernel_tmp_buf).unwrap_success();
         f.close();
 
         use elf_rs::*;
-        let elf = Elf::from_bytes(&tmp_buf).unwrap();
+        let elf = Elf::from_bytes(&kernel_tmp_buf).unwrap();
         let mut kernel_start = u64::max_value();
         let mut kernel_end = u64::min_value();
         if let Elf::Elf64(ref e) = elf {
@@ -91,18 +91,24 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
 
         let load_len = kernel_end - kernel_start;
         let n_pages = (load_len as usize + 0xfff) / 0x1000;
-        let p = boot_services
+        let kernel_p = boot_services
             .allocate_pages(
+                // NOTE: The starting address should be aligned
                 AllocateType::Address(kernel_start as usize),
                 MemoryType::LOADER_DATA,
                 n_pages,
             )
             .unwrap_success();
+        // writeln!(st.stdout(), "allocated kernel region");
 
-        let mut buf = unsafe { core::slice::from_raw_parts_mut(p as *mut u8, load_len as usize) };
+        let mut kernel_buf = unsafe { core::slice::from_raw_parts_mut(kernel_p as *mut u8, load_len as usize) };
 
         // Zeros the kernel region
-        unsafe { core::ptr::write_bytes(&mut buf, 0, load_len as usize) };
+        // unsafe { core::ptr::write_bytes(&mut kernel_buf, 0, load_len as usize) };
+        for i in 0..load_len {
+            kernel_buf[i as usize] = 0;
+        }
+        writeln!(st.stdout(), "zeroed");
 
         // Read kernel file into the memory
         if let Elf::Elf64(ref e) = elf {
@@ -110,23 +116,26 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
                 let header = p.ph;
                 if matches!(header.ph_type(), ProgramType::LOAD) {
                     let src = p.segment();
-                    let dst = header.vaddr();
+                    let addr = header.vaddr();
                     let len = header.filesz();
-                    unsafe { core::ptr::copy(&src, dst as *mut &[u8], len as usize) };
+                    let mut dst = unsafe { core::slice::from_raw_parts_mut(addr as *mut u8, len as usize) };
+                    for i in 0..len as usize{
+                        dst[i] = src[i];
+                    }
                 }
             }
         }
 
-        boot_services.free_pool(tmp_p);
 
         writeln!(st.stdout(), "kernel is read into the memory").unwrap();
 
         // Entry address should be found at +24
-        let buf = unsafe { core::slice::from_raw_parts((p + 24) as *mut u8, 8)};
+        let buf = unsafe { core::slice::from_raw_parts((kernel_tmp_p as u64 + 24) as *mut u8, 8)};
         let kernel_main_addr = LittleEndian::read_u64(&buf);
         writeln!(st.stdout(), "kernel_main address = {:x}", kernel_main_addr).unwrap();
 
-        st.exit_boot_services(handle, &mut tmp_buf).unwrap_success();
+        boot_services.free_pool(kernel_tmp_p);
+        st.exit_boot_services(handle, &mut misc_buf).unwrap_success();
 
         let kernel_main = unsafe {
             let f: extern "efiapi" fn(&FrameBufferConfig) -> ! = core::mem::transmute(kernel_main_addr);
