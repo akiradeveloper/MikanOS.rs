9cba75139953dda3153c3295d9bd1098ecf763a3
Akira Hayakawa <ruby.wktk@gmail.com>
2021-04-04 19:55:33 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ kernel/src/graphics.rs
@@ -0,0 +1,32 @@
+use mikan::{FrameBufferConfig, PixelFormat};
+#[derive(Clone, Copy)]
+pub struct PixelColor {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+fn write_pixel_triple(fb_config: &FrameBufferConfig, x: u32, y: u32, abc: (u8,u8,u8)) {
+    let pixel_pos = fb_config.pixels_per_scan_line * y + x;
+    let (c0,c1,c2) = abc;
+    let fb_addr = fb_config.frame_buffer;
+    let fb_size = 4 * fb_config.pixels_per_scan_line * fb_config.vertical_resolution;
+    let fb = unsafe { core::slice::from_raw_parts_mut(fb_addr, fb_size as usize) };
+    let base = 4 * pixel_pos as usize;
+    fb[base+0] = c0;
+    fb[base+1] = c1;
+    fb[base+2] = c2;
+}
+
+pub fn write_pixel(x: u32, y: u32, color: PixelColor) {
+    let fb_config = unsafe { crate::G_CONTEXT.fb_config() };
+    let (c0,c1,c2) = match fb_config.pixel_format {
+        PixelFormat::RGBResv8BitPerColor => {
+            (color.g, color.b, color.r)
+        },
+        PixelFormat::BGRResv8BitPerColor => {
+            (color.b, color.g, color.r)
+        },
+    };
+    write_pixel_triple(&fb_config, x, y, (c0, c1, c2));
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,8 +6,10 @@
 
 extern crate panic_halt;
 mod fonts;
+mod graphics;
 
 use mikan::{FrameBufferConfig, PixelFormat};
+use graphics::{write_pixel, PixelColor};
 
 struct Context {
     fb_config: Option<FrameBufferConfig>,
@@ -20,38 +22,6 @@ impl Context {
 static mut G_CONTEXT: Context = Context {
     fb_config: None,
 };
-
-#[derive(Clone, Copy)]
-struct PixelColor {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-fn write_pixel_triple(fb_config: &FrameBufferConfig, x: u32, y: u32, abc: (u8,u8,u8)) {
-    let pixel_pos = fb_config.pixels_per_scan_line * y + x;
-    let (c0,c1,c2) = abc;
-    let fb_addr = fb_config.frame_buffer;
-    let fb_size = 4 * fb_config.pixels_per_scan_line * fb_config.vertical_resolution;
-    let fb = unsafe { core::slice::from_raw_parts_mut(fb_addr, fb_size as usize) };
-    let base = 4 * pixel_pos as usize;
-    fb[base+0] = c0;
-    fb[base+1] = c1;
-    fb[base+2] = c2;
-}
-
-fn write_pixel(x: u32, y: u32, color: PixelColor) {
-    let fb_config = unsafe { G_CONTEXT.fb_config() };
-    let (c0,c1,c2) = match fb_config.pixel_format {
-        PixelFormat::RGBResv8BitPerColor => {
-            (color.g, color.b, color.r)
-        },
-        PixelFormat::BGRResv8BitPerColor => {
-            (color.b, color.g, color.r)
-        },
-    };
-    write_pixel_triple(&fb_config, x, y, (c0, c1, c2));
-}
 
 #[no_mangle]
 extern "efiapi" fn kernel_main(fb_config: FrameBufferConfig) -> ! {
